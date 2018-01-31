aa9f7e706709b354a15877feca5cad14061f43bf
Adrian Polo Alcaide <adri@iMac-de-Adrian.Home>
2018-01-30 16:05:11 -0800

    Changed to point

--- src/app/models/board.ts
+++ src/app/models/board.ts
@@ -1,19 +1,21 @@
+import {Point} from './point';
+
 export class Board {
-  public board: number[][];
+  public board: Point[][];
 
   constructor(size: number) {
     this.board = [];
     for (let _i = 0; _i < size; _i++) {
-      this.board.push(new Array<number>(10));
+      this.board.push(new Array<Point>(10));
     }
-    this.board.forEach(arr => arr.fill(0));
+    this.board.forEach(arr => arr.fill(new Point(0)));
   }
 
   increaseValue(i: number, j: number) {
-    if (this.board[i][j] >= 5){
-      this.board[i][j] = 0;
-    }else{
-      this.board[i][j] += 1;
+    if (this.board[i][j] >= 5) {
+      this.board[i][j].value += 1;
+    } else {
+      this.board[i][j].value += 1;
     }
   }
 
